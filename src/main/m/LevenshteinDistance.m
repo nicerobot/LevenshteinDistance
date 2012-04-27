b913ac2a621883502945567772c5d6af7373ab46
nicerobot <git@nicerobot.org>
2012-04-27 16:10:19 -0400

    .

--- src/main/m/LevenshteinDistance.m
+++ src/main/m/LevenshteinDistance.m
@@ -6,44 +6,6 @@
 //
 #import "LevenshteinDistance.h"
 
-int LevenshteinDistance1(NSString *from, NSString *to)
-{
-  int m = (int)[from length];
-  int n = (int)[to length];
-  int d[m+1][n+1];
-  
-  for (int i=0; i<=m; i++) {
-    d[i][0] = i;
-  }
-  for (int j=0; j<=n; j++) {
-    d[0][j] = j;
-  }
-  
-  for (int i=1; i<=m; i++) {
-    for (int j=1; j<=n; j++) {
-      int r;
-      int pi=i-1;
-      int pj=j-1;
-      int i1j1 = d[pi][pj];
-      int b = [from characterAtIndex:pi] == [to characterAtIndex:pj];
-      //printf("\t(%d",b);
-      if (b) {
-        r = i1j1;
-      } else {
-        int ij1 = d[i][pj];
-        int i1j = d[pi][j];
-        r = MIN(i1j,MIN(ij1,i1j1)) + 1;
-        //printf(",%02d,%02d",ij1,i1j);
-      }
-      d[i][j] = r;
-      //printf(")%02d",r);
-    }
-    //printf("\n");
-  }
-  
-  return d[m][n];
-}
-
 int LevenshteinDistance(NSString *from, NSString *to)
 {
   int m = (int)[from length];
--- src/main/m/main.m
+++ src/main/m/main.m
@@ -9,8 +9,7 @@ int main (int argc, const char * argv[])
     NSString *first = [NSString stringWithUTF8String:argv[1]];
     for (int i=2; i<argc; i++) {
       NSString *to = [NSString stringWithUTF8String:argv[i]];
-      printf("=\t%s\t%s\t%d\n",argv[1],argv[i],LevenshteinDistance(first,to));
-      printf("_\t%s\t%s\t%d\n\n",argv[1],argv[i],LevenshteinDistance1(first,to));
+      printf("%s\t%s\t%d\n",argv[1],argv[i],LevenshteinDistance(first,to));
     }
   }
   return 0;
