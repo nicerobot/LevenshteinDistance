5d0c1232498109b505148b48385b1cbf848b1623
nicerobot <git@nicerobot.org>
2012-05-01 10:18:28 -0400

    reverted bugged optimizations.

--- src/main/m/main.m
+++ src/main/m/main.m
@@ -9,9 +9,7 @@ int main (int argc, const char * argv[])
     NSString *first = [NSString stringWithUTF8String:argv[1]];
     for (int i=2; i<argc; i++) {
       NSString *to = [NSString stringWithUTF8String:argv[i]];
-      printf("?M %s\t%s\t%d\n",argv[1],argv[i],LevenshteinDistanceM(first,to));
-      printf("?N %s\t%s\t%d\n",argv[1],argv[i],LevenshteinDistanceN(first,to));
-      printf("== %s\t%s\t%d\n\n",argv[1],argv[i],LevenshteinDistance(first,to));
+      printf("%s\t%s\t%d\n",argv[1],argv[i],LevenshteinDistance(first,to));
     }
   }
   return 0;
