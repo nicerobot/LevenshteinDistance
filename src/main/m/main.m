8ad3d4c51b0521d16410b200459cff24aee1089f
nicerobot <git@nicerobot.org>
2012-05-08 23:07:00 -0400

    removed optimizations.

--- src/main/m/main.m
+++ src/main/m/main.m
@@ -12,12 +12,8 @@ int main (int argc, const char * argv[])
     for (int i=2; i<argc; i++) {
       NSString *to = [NSString stringWithUTF8String:argv[i]];
       [sorted addObject:to];
-      printf("= %s\t%s\t%d\n",argv[1],argv[i],[[to distanceFrom:first] intValue]);
-      printf("N %s\t%s\t%d\n\n",argv[1],argv[i],LevenshteinDistanceN(first,to));
+      printf("%s\t%s\t%d\n",argv[1],argv[i],[[to distanceFrom:first] intValue]);
     }
-    NSLog(@"%@",sorted);
-    [sorted sortUsingComparator:[first distanceComparator]];
-    NSLog(@"%@",sorted);
     NSRunLoop *loop = [NSRunLoop currentRunLoop];
     int done = NO;
     while (!done && [loop runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:1.0]]) done=YES;
